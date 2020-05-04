8920b850bb3ac30b4272f5d340719ca7888cb654
Sophia Pearson <scpearson94@byui.edu>
2020-05-03 22:39:49 -0700

    add navbar and additional pages

--- web/homepage.php
+++ web/homepage.php
@@ -13,12 +13,11 @@
     <div id="homeHeader">
     <!--Navigation bar-->
     <ul id="navbar">
-        <li class="navitem"><a class="active" href="homepage.html">Home</a></li>
+        <li class="navitem"><a class="active" href="homepage.php">Home</a></li>
         <li class="navitem"><a href="w02/aboutme.html">About Me</a></li>
         <li class="navitem"><a href="assignmentList.html">Assignments</a></li>
         <li class="navitem"><a href="w02/randomFacts.html">Random Facts</a></li>
     </ul>
-
     
     <h1><section id="headerName">Sophia Pearson</section>HOME PAGE</h1>
     </div>
@@ -26,12 +25,19 @@
     <div id="body-content">
     <h2>Home Page for CSE 341</h2>
 
+    <?php
+
+        echo "The time is " . date("h:i:sa");
+
+    ?>
+
     </div>
     
     <footer>
         <p>Posted by: Sophia Pearson</p>
         <p>Contact information: <a href="mailto:scpearon94@byui.edu">
         scpearon94@byui.edu</a>.</p>
+        <p>&copy; 2010-<?php echo date("Y");?></p>
     </footer>
 </body>
 </html>
