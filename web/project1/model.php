7105d8caac338dfa01f1d887ace8dbbba4b8d69c
Sophia Pearson <scpearson94@byui.edu>
2020-05-23 22:48:57 -0700

    update model.php in project 1

--- web/project1/model.php
+++ web/project1/model.php
@@ -3,9 +3,10 @@
 function getBrowseList () {
     foreach ($db->query('SELECT name, price, image_url FROM product') as $row)
     {
-    echo 'name: ' . $row['username'];
-    echo ' price: ' . $row['password'];
-    echo ' image_url: ' . $row['password'];
+    echo 'name: ' . $row['name'];
+    echo ' price: ' . $row['price'];
+    echo ' image_url: ' . $row['image_url'];
+    echo ' product type id: ' . $row['product_type_id'];
     echo '<br/>';
     }
 }
