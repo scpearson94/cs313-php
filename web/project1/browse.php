e781712f16d3b4d058a99c65f0bc7d0beacd9b37
Sophia Pearson <scpearson94@byui.edu>
2020-05-26 14:41:46 -0700

    add debugging code to project 1

--- web/project1/browse.php
+++ web/project1/browse.php
@@ -39,25 +39,8 @@
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
     <!--inventory table-->
         <?php 
-            function getBrowseList () {
-                echo "we made it to the model funtion";
-                $stmt = $db->prepare("SELECT * FROM table WHERE price=:price AND name=:name");
-                echo "we made it past the statement declaratrion";
-                $stmt->execute(array(':name' => $name, ':price' => $price));
-                echo "we made it past the statement execution";
-            
-                while ($rows = $stmt->fetchAll(PDO::FETCH_ASSOC))
-                {
-                    echo "no bug here model2!";
-                    echo 'name: ' . $name;
-                    echo ' price: ' . $price;
-                    //echo ' image_url: ' . $row['image_url'];
-                    //echo ' product type id: ' . $row['product_type_id'];
-                    echo '<br/>';
-                }
-                echo "no bug here model4!";
-            } 
-            getBrowseList();
+            include "model.php"; 
+            getBrowseList($db);
         ?>
     </form>
         <?php echo "no bug in browse!"; ?>
