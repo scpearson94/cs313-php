eeff1a1d24b06d87c7abe50a975358a56f6b1f28
Sophia Pearson <scpearson94@byui.edu>
2020-05-26 14:37:32 -0700

    add debugging code to project 1

--- web/project1/model.php
+++ web/project1/model.php
@@ -1,22 +1,22 @@
 <?php
 echo "no bug here model1!";
 function getBrowseList () {
-    $statement = $db->prepare("SELECT product_type_id, name, price, image_url FROM product");
-    $statement->execute();
-    if (isset($statement)) {
-        echo "statement is set";
-    } else {
-        echo "statement is not set";
-    }
-    while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+    echo "we made it to the model funtion";
+    $stmt = $db->prepare("SELECT * FROM table WHERE price=:price AND name=:name");
+    echo "we made it past the statement declaratrion";
+    $stmt->execute(array(':name' => $name, ':price' => $price));
+    echo "we made it past the statement execution";
+
+    while ($rows = $stmt->fetchAll(PDO::FETCH_ASSOC))
     {
         echo "no bug here model2!";
-        echo 'name: ' . $row['name'];
-        echo ' price: ' . $row['price'];
-        echo ' image_url: ' . $row['image_url'];
-        echo ' product type id: ' . $row['product_type_id'];
+        echo 'name: ' . $name;
+        echo ' price: ' . $price;
+        //echo ' image_url: ' . $row['image_url'];
+        //echo ' product type id: ' . $row['product_type_id'];
         echo '<br/>';
     }
+    echo "no bug here model4!";
 }
 
 echo "no bug here model3!";
