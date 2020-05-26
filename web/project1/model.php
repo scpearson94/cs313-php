fa72630dbcb62790ee09fe8d653c2efb95f96f10
Sophia Pearson <scpearson94@byui.edu>
2020-05-26 14:53:41 -0700

    fix bug in model.php

--- web/project1/model.php
+++ web/project1/model.php
@@ -2,16 +2,15 @@
 echo "no bug here model1!";
 function getBrowseList ($db) {
     echo "we made it to the model funtion";
-    $stmt = $db->prepare("SELECT * FROM product WHERE price=:price AND name=:name");
+    $statement = $db->prepare("SELECT name, price, image_url FROM product");
     echo "we made it past the statement declaratrion";
-    $stmt->execute(array(':name' => $name, ':price' => $price));
+    $statement->execute();
     echo "we made it past the statement execution";
 
-    while ($rows = $stmt->fetchAll(PDO::FETCH_ASSOC))
-    {
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
         echo "no bug here model2!";
-        echo 'name: ' . $name;
-        echo ' price: ' . $price;
+        echo 'name: ' . $row['name'];
+        echo ' price: ' . $row['price'];
         //echo ' image_url: ' . $row['image_url'];
         //echo ' product type id: ' . $row['product_type_id'];
         echo '<br/>';
