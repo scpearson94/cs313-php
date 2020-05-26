67bafe5757ee73daa0084772c6e93c353e5a0fff
Sophia Pearson <scpearson94@byui.edu>
2020-05-26 14:55:39 -0700

    take out debugging code project 1

--- web/project1/model.php
+++ web/project1/model.php
@@ -1,21 +1,13 @@
 <?php
-echo "no bug here model1!";
+
 function getBrowseList ($db) {
-    echo "we made it to the model funtion";
     $statement = $db->prepare("SELECT name, price, image_url FROM product");
-    echo "we made it past the statement declaratrion";
     $statement->execute();
-    echo "we made it past the statement execution";
 
     while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-        echo "no bug here model2!";
         echo 'name: ' . $row['name'];
         echo ' price: ' . $row['price'];
-        //echo ' image_url: ' . $row['image_url'];
-        //echo ' product type id: ' . $row['product_type_id'];
+        echo ' image_url: ' . $row['image_url'];
         echo '<br/>';
     }
-    echo "no bug here model4!";
 }
-
-echo "no bug here model3!";
