32fd7ce916a42095f69d0b12aaf886c86d3913a5
Sophia Pearson <scpearson94@byui.edu>
2020-06-04 14:59:21 -0700

    update image properties for project 1

--- web/project1/model.php
+++ web/project1/model.php
@@ -3,15 +3,17 @@
 function getBrowseList ($db) {
     $statement = $db->prepare("SELECT name, price, image_url FROM product");
     $statement->execute();
+    $output = "";
 
     while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-        $output = "";
+        
         $output .= "<div class='browse_item'><div class='image_container'>";
         $output .= "<img src='images/" . $row['image_url'] . "' alt='" . $row['name'] . "'>";
         $output .= "</div><section class='image_descrip'><div class='item_name'>";
         $output .= $row['name'] . "</div><div class='item_price'>";
         $output .= $row['price'] . "</div></section></div>";
 
-        echo $output;
     }
+
+    echo $output;
 }
