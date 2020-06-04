1fd2325c0f03771c33b428481d21e49a78604f9d
Sophia Pearson <scpearson94@byui.edu>
2020-06-04 15:16:17 -0700

    update test page for project 1

--- web/project1/heading.php
+++
@@ -1,7 +0,0 @@
-<?php
-<!--Navigation bar-->
-    <ul id="navbar">
-        <li class="navitem"><a <?php if ($currentPage === 'Home') {echo 'class="active"';} ?> href="home.php">Home</a></li>
-        <li class="navitem"><a <?php if ($currentPage === 'Browse') {echo 'class="active"';} ?> href="browse.php">Inventory</a></li>
-        <li class="lastNavItem"><a <?php if ($currentPage === 'Cart') {echo 'class="active"';} ?> href="cart.php">View Cart</a></li>
-    </ul>
