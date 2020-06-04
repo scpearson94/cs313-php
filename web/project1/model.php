8282b52a6fcce0aafc5845cdd69b98494c48a37b
Sophia Pearson <scpearson94@byui.edu>
2020-06-04 14:27:19 -0700

    update browse for project 1

--- web/project1/model.php
+++ web/project1/model.php
@@ -10,7 +10,7 @@ function getBrowseList ($db) {
         $output .= "<img src='images/" . $row['image_url'] . "' alt='" . $row['name'] . "'>";
         $output .= "</div><section class='image_descrip'><div class='item_name'>";
         $output .= $row['name'] . "</div><div class='item_price'>";
-        $output .= "</div></section></div>" . $row['price'];
+        $output .= $row['price'] . "</div></section></div>";
 
         echo $output;
     }
