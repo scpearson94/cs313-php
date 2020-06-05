583e60414d19f823ad8198cbffaff03e56a846ca
Sophia Pearson <scpearson94@byui.edu>
2020-06-04 23:03:41 -0700

    add debugging code to project 1

--- web/project1/model.php
+++ web/project1/model.php
@@ -48,24 +48,28 @@
 
 
 function getBrowseList ($db) {
+    echo "now in get list function";
     $productArray = array();
     $statement = $db->prepare("SELECT name, price, image_url FROM product");
     $statement->execute();
+    echo "statement is prepared";
 
     while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
         $product = new Product($row['name'], $row['price'], $row['image_url']);
         array_push($productArray, $product);
     }
+    echo "made it past the while loop";
 
     print($productArray);
     displayBrowseList($productArray);
 }
 
 function displayBrowseList ($productArray) {
+    echo " now in display browse list";
     $output = "";
 
     foreach ($productArray as $product) {
-
+        echo "in the foreach loop";
         $output .= "<div class='browse_item'><div class='image_container'>";
         $output .= "<img src='images/" . $product->image_url . "' alt='" . $product->name . "'>";
         $output .= "</div><section class='image_descrip'><div class='item_name'>";
