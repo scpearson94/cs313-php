6f30c191497712bbd12ae4503c8a7392da11271e
Sophia Pearson <scpearson94@byui.edu>
2020-06-09 02:37:26 -0700

    update confirmation page

--- web/project1/model.php
+++ web/project1/model.php
@@ -49,9 +49,10 @@ function submitOrder ($db, $first_name, $last_name, $email, $address) {
         $id = ltrim($product->get_product_id(), "_");
         $amount = $product->get_quantity();
         echo "7 ";
-        $statement = $db->prepare("INSERT INTO cart (product_id, amount, order_id) VALUES (:id, :amount)");
+        $statement = $db->prepare("INSERT INTO cart (product_id, amount, order_id) VALUES (:id, :amount, :newId)");
         $statement->bindValue(':id', $id);
         $statement->bindValue(':amount', $amount);
+        $statement->bindValue(':newId', $newId);
         $statement->execute();
         echo "8 ";
     }
