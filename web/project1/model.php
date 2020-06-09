fc9e96176db14c8fffd15e8c2dfdc3496c89a856
Sophia Pearson <scpearson94@byui.edu>
2020-06-08 21:54:30 -0700

    update cart for prj1

--- web/project1/model.php
+++ web/project1/model.php
@@ -1,16 +1,15 @@
 <?php
 
 function getBrowseList ($db) {
-    echo $query = "SELECT * FROM product";
-    exit;
+    $query = "SELECT product_type_id, name, price, image_url FROM product";
     $statement = $db->prepare($query);
     $statement->execute();
     $output = "";
     $productArray = array();
+    $product_id = 1;
 
     while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
         $name = $row['name'];
-        $product_id = $row['id'];
         $product = new Product($name, $row['price'], $row['image_url'], $product_id);
         array_push($productArray[$product_id]);
         $productArray[$product_id] = $product;
@@ -21,6 +20,7 @@ function getBrowseList ($db) {
         $output .= $name . "</div><div class='item_price'>$";
         $output .= $row['price'] . "</div></section><section class='addToCartSct'><input type='submit' class='addToCartBtn' name='" . $product_id . "' value='Add to Cart'/></section></div>";
 
+        $product_id++;
     }
 
     print_r($_SESSION);
