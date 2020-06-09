6e05117108a60b01deedec0296df7bae5d0b3414
Sophia Pearson <scpearson94@byui.edu>
2020-06-08 17:50:55 -0700

    add button to browse page

--- web/project1/model.php
+++ web/project1/model.php
@@ -14,7 +14,7 @@ function getBrowseList ($db) {
         $output .= "<img src='images/" . $row['image_url'] . "' alt='" . $row['name'] . "'>";
         $output .= "</div><section class='image_descrip'><div class='item_name'>";
         $output .= $row['name'] . "</div><div class='item_price'>";
-        $output .= $row['price'] . "</div></section></div>";
+        $output .= $row['price'] . "</div></section><section><input type='submit' class='addToCartBtn' name='" . $row['name'] . "' value='Add to Cart'/></section></div>";
 
     }
 
