9be4a2e051ff9df4ef454771e5a7952f2deb02c1
Sophia Pearson <scpearson94@byui.edu>
2020-06-09 16:08:16 -0700

    add select tag to browse page

--- web/project1/browse.php
+++ web/project1/browse.php
@@ -101,20 +101,22 @@
 
     <h2>Current Inventory</h2>
     
+    <?php
+        include "model.php";
+    ?>
+        
     <form class="content" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
         <!--filter dropdown-->
         <label for="filter">Filter:</label>
         <select name="filter" id="filter">
             <option value="default">--No filter selected--</option>
             <?php
-                //include "model.php";
-                //displayFilterList();
+                displayFilterList();
             ?>
         </select>
         <!--inventory table-->
         <div class="content">
             <?php 
-                include "model.php"; 
                 $productArray = getBrowseList();
                 if($_SERVER['REQUEST_METHOD'] == 'POST') {
                     $myPost = array_keys($_POST);
