68f9e819b2a07f266a68dc82767b3b21be7c5717
Sophia Pearson <scpearson94@byui.edu>
2020-06-08 17:40:21 -0700

    reverting changes to browse in project 1

---
+++ web/project1/model copy.php
@@ -0,0 +1,37 @@
+<?php
+
+function getBrowseList ($db) {
+    echo "now in get list function";
+    $productArray = array();
+    $statement = $db->prepare("SELECT name, price, image_url FROM product");
+    $statement->execute();
+    echo "statement is prepared";
+
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        $product = new Product($row['name'], $row['price'], $row['image_url']);
+        array_push($productArray, $product);
+    }
+    echo "made it past the while loop";
+
+    print_r($productArray);
+    displayBrowseList($productArray);
+}
+
+function displayBrowseList ($productArray) {
+    echo " now in display browse list";
+    $output = "";
+
+    foreach ($productArray as $product) {
+        echo "in the foreach loop";
+        $output .= "<div class='browse_item'><div class='image_container'>";
+        $output .= "<img src='images/" . $product->image_url . "' alt='" . $product->name . "'>";
+        $output .= "</div><section class='image_descrip'><div class='item_name'>";
+        $output .= $product->name . "</div><div class='item_price'>$";
+        $output .= $product->price . "</div></section>";
+        $output .= "<input type='submit' class='addToCartBtn' name='" . $product->name . "' value='Add to Cart'/></div>";
+
+    }
+
+    print_r($productArray);
+    echo $output;
+}
--- web/project1/model.php
+++ web/project1/model.php
@@ -1,37 +1,22 @@
 <?php
 
 function getBrowseList ($db) {
-    echo "now in get list function";
-    $productArray = array();
     $statement = $db->prepare("SELECT name, price, image_url FROM product");
     $statement->execute();
-    echo "statement is prepared";
-
-    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-        $product = new Product($row['name'], $row['price'], $row['image_url']);
-        array_push($productArray, $product);
-    }
-    echo "made it past the while loop";
-
-    print_r($productArray);
-    displayBrowseList($productArray);
-}
-
-function displayBrowseList ($productArray) {
-    echo " now in display browse list";
     $output = "";
 
-    foreach ($productArray as $product) {
-        echo "in the foreach loop";
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        
+        $product = new Product($row['name'], $row['price'], $row['image_url']);
+        array_push($productArray, $product);
+
         $output .= "<div class='browse_item'><div class='image_container'>";
-        $output .= "<img src='images/" . $product->image_url . "' alt='" . $product->name . "'>";
+        $output .= "<img src='images/" . $row['image_url'] . "' alt='" . $row['name'] . "'>";
         $output .= "</div><section class='image_descrip'><div class='item_name'>";
-        $output .= $product->name . "</div><div class='item_price'>$";
-        $output .= $product->price . "</div></section>";
-        $output .= "<input type='submit' class='addToCartBtn' name='" . $product->name . "' value='Add to Cart'/></div>";
+        $output .= $row['name'] . "</div><div class='item_price'>";
+        $output .= $row['price'] . "</div></section></div>";
 
     }
 
-    print($productArray);
     echo $output;
 }
